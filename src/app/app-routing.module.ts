41dfadde44128accdb9a062e4f6170c46aca5d88
Opibl <pedro.basualto.7@gmail.com>
2021-04-28 12:13:22 -0400

    cambio2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarComponent } from './pages/editar/editar.component';
+import { Figura1Component } from './pages/figura1/figura1.component';
+import { Figura2Component } from './pages/figura2/figura2.component';
+import {SemaforoComponent} from './pages/semaforo/semaforo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:Figura1Component},
+  {path:'editar',component:EditarComponent},
+  {path:'figura2',component:Figura2Component},
+  {path:'semaforo',component:SemaforoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Figura1Component } from './pages/figura1/figura1.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { EditarComponent } from './pages/editar/editar.component';
+import { Figura2Component } from './pages/figura2/figura2.component';
+import { SemaforoComponent } from './pages/semaforo/semaforo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Figura1Component,
+    EditarComponent,
+    Figura2Component,
+    SemaforoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
